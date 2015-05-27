d8044fb3d6533732ca506b436b4ebd3dc32fdc7e
David Catuhe <david.catuhe@live.fr>
2015-05-27 09:39:58 -0700

    Sample function

---
+++ sample.js
@@ -0,0 +1,7 @@
+// Author: @deltakosh
+// Purpose: This is a sample function to show how to add features in this repo
+// Babylon.js version used: 2.1
+
+function veryUsefulFunctionIWantToShare() {
+	return 42;
+}
